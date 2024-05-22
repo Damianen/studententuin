08b9bb3d392ecd7949861346ceee4c7736b73f8c
Coen <kruijf.coen@gmail.com>
2024-05-22 13:13:27 +0200

    add static html page

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('<h1> Hello World!? </h1> ')
-})
+app.set("view options", {layout: false});
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.render('index.html');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
