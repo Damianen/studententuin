5da78ef16ec46d0b1140e476c528a89c5f8be59a
Damianen <113175544+Damianen@users.noreply.github.com>
2024-05-23 09:21:55 +0200

    Add ES6 with modules

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express, { static as _static } from 'express';
+import { fileURLToPath } from 'url'
+import path from 'path'
+const app = express();
+const port = 3000;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 app.set("view options", {layout: false});
-app.use(express.static(__dirname + '/public'));
+app.use(_static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
     res.render('index.html');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
