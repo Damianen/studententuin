69f73a9c8c8017bde1dd70f16ab77a02ff6fcd03
Coen <kruijf.coen@gmail.com>
2024-05-27 16:42:30 +0200

    add global colors

--- src/index.js
+++ src/index.js
@@ -6,5 +6,7 @@ import '../public/style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
+    <div class="bg-primary-green">
+    <h1>Hello World</h1>
+  </div>
 );
