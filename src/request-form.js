ee7b938eb3d2a2f6caee8ac4caa07da667211881
RSruijlaart <r@struijlaart.nl>
2024-05-27 11:02:32 +0200

    added request form

---
+++ src/request-form.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+export default function RequestForm() {
+
+    return (
+
+        <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Subdomein aanvragen</h2>
+            </div>
+
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form className="space-y-6" action="#" method="POST">
+                    <div>
+                        <label htmlFor="subdomainName" className="block text-sm font-medium leading-6 text-gray-900">Subdomein Naam</label>
+                        <div className="mt-2">
+                            <input id="subdomainName" name="subdomainName" type="subdomainName" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"/>
+                        </div>
+                    </div>
+
+                    <div>
+                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+                        <div className="mt-2">
+                            <input id="email" name="email" type="email" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"/>
+                        </div>
+                    </div>
+
+                    <div>
+                        <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>    
+                        <div className="mt-2">
+                            <input id="password" name="password" type="password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"/>
+                        </div>
+                    </div>
+
+                    <div>
+                        <label htmlFor="productPackage" className="block text-sm font-medium leading-6 text-gray-900">Product pakket</label>    
+                        <div className="mt-2">
+                            <select name="productPackage" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6" id="productPackage">
+                                <option value="">Kies een pakket</option>
+                                <option value="free">Gratis</option>
+                                <option value="basic">Basis</option>
+                                <option value="premium">Premium</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div>
+                        <button type="submit" className="flex w-full justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600">Aanvraag opsturen</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+    );
+};
