05918587404906c3021ffd25fb909ae5d35e4da0
Zaid Karmoudi <zaidkarmoudi@gmail.com>
2024-05-27 13:41:40 +0200

    add homepage

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import "../public/style.css";
 import Home from "./home.js";
 import Navigation from "./Navigation.js";
 import Footer from "./Footer.js";
-import Explanation from "./explanation.js";
+import Homepage from "./Homepage.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
     <Navigation />
-    <Explanation />
+    <Homepage />
     <Footer />
   </>
 );
