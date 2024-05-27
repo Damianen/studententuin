87485294553ac1f0db0dd99a83d20f8371b9cca3
Dayal-Manbodh <151162763+Dayal-Manbodh@users.noreply.github.com>
2024-05-27 12:57:41 +0200

    dashboard

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "../public/style.css";
 
 import Login from "./login.js";
+import Dashboard from "./dashboard.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Login />);
+root.render(<Dashboard />);
