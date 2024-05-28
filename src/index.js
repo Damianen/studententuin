d56c19ead74f43ab393ccb73124007e0fc0b66f2
Zaid Karmoudi <zaidkarmoudi@gmail.com>
2024-05-28 09:26:57 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,20 +5,13 @@ import "../public/style.css";
 import Home from "./home.js";
 import Navigation from "./Navigation.js";
 import Footer from "./Footer.js";
-import Homepage from "./homepage.js";
+import Homepage from "./Homepage.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-<<<<<<< HEAD
   <>
     <Navigation />
     <Homepage />
     <Footer />
   </>
 );
-=======
-    <div class="bg-primary-green">
-    <h1>Hello World</h1>
-  </div>
-);
->>>>>>> main
