fe0a1c49b1472a345c7454b7de87cfd205622d83
Coen <kruijf.coen@gmail.com>
2024-05-30 11:49:28 +0200

    change contact page

--- src/contact.js
+++ src/contact.js
@@ -1,62 +1,38 @@
 import React from "react";
-// import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Contact = (props) => {
   return (
-    <section class="bg-white">
-      <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-        <h2 class="mb-4 text-4xl tracking-tight text-center">Contact Us</h2>
-        <p class="mb-8 lg:mb-16 font-light text-center sm:text-xl">
-          Got a technical issue? Want to send feedback about a beta feature?
-          Need details about our Business plan? Let us know.
-        </p>
-        <div class="lg:p-28 md:p-36 sm:20 p-4 w-full shadow-md border border-gray-200 rounded-lg">
-          <form action="#" class="space-y-8">
-            <div>
-              <label for="email" class="block mb-2 text-sm font-medium">
-                Your email
-              </label>
-              <input
-                type="email"
-                id="email"
-                class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-green-400"
-                placeholder="name@flowbite.com"
-                required
-              ></input>
-            </div>
-            <div>
-              <label for="subject" class="block mb-2 text-sm font-medium ">
-                Subject
-              </label>
-              <input
-                type="text"
-                id="subject"
-                class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-green-400"
-                placeholder="Let us know how we can help you"
-                required
-              ></input>
-            </div>
-            <div class="sm:col-span-2">
-              <label for="message" class="block mb-2 text-sm font-medium ">
-                Your message
-              </label>
-              <textarea
-                id="message"
-                rows="6"
-                class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-green-400"
-                placeholder="Leave a comment..."
-              ></textarea>
-            </div>
-            <button
-              type="submit"
-              class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-green sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-            >
-              Send message
-            </button>
-          </form>
-        </div>
+    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 space-y-2">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Contact</h2>
       </div>
-    </section>
+       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm shadow-xl rounded-lg p-10 border border-gray-200">
+          <form className="space-y-4" action="#" method="POST">
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Jouw Email</label>
+                <div className="mt-2">
+                  <input id="email" name="email" type="email" required className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-green sm:text-sm sm:leading-6 focus:outline-none"/>
+                </div>
+              </div>
+            <div>
+              <label htmlFor="subject" className="block text-sm font-medium leading-6 text-gray-900">Onderwerp</label>    
+              <div className="mt-2">
+                <input id="text" name="subject" type="text" required className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-green sm:text-sm sm:leading-6 focus:outline-none"/>
+              </div>
+            </div>
+            <div>
+              <label htmlFor="subject" className="block text-sm font-medium leading-6 text-gray-900">Jouw Bericht</label>    
+              <div className="mt-2">
+                <textarea id="text" name="subject" type="text" required className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-green sm:text-sm sm:leading-6 focus:outline-none"> </textarea>
+              </div>
+            </div>
+            <div className="flex justify-end">
+              <button type="submit" className="flex justify-center rounded-md bg-primary-green px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-house-green focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-house-green focus:outline-none self-end">Stuur bericht</button>
+            </div>
+        </form>
+      </div>
+    </div>
   );
 };
 
