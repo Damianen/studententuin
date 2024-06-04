b1e1d8bca0500e2f700d71cd178e080e738a3bf7
Coen <kruijf.coen@gmail.com>
2024-06-04 14:33:34 +0200

    add jwt token to login

--- server/router.js
+++ server/router.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import bcrypt from "bcrypt";
 import userService from "../server/services/user.service.js";
 import cors from "cors";
+import jwt from "jsonwebtoken";
 
 const router = Router();
 
@@ -54,17 +55,23 @@ router.post("/login", async (req, res) => {
 
     const passwordMatch = await bcrypt.compare(password, user.password);
 
+    const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET
+    );
+
     // Als de wachtwoorden niet overeenkomen, geef een foutmelding terug
 
     if (!passwordMatch) {
       return res.status(401).json({ message: "Invalid email or password" });
     }
 
+    res.cookie('token', token ,{httpOnly: true, secure: true})
+
     // Als de inloggegevens correct zijn, sla de gebruiker op in de sessie
     req.session = user;
 
     // Geef een succesbericht terug
     res.json({ message: "Login successful" });
+    res.redirect("/manage");
   } catch (error) {
     console.error("Error:", error);
     res.status(500).json({ message: "An error occurred" });
