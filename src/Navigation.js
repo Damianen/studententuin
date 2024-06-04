83b886b98f6c62329c49405c6ba96a723d933723
RSruijlaart <r@struijlaart.nl>
2024-06-04 13:25:00 +0200

    quickfix

--- src/Navigation.js
+++ src/Navigation.js
@@ -63,18 +63,6 @@ const handleJoyrideCallback = (data = null) => {
   }
 };
 
-const joyrideStyling = {
-  arrowColor: '#fff',
-  backgroundColor: '#fff',
-  beaconSize: 36,
-  overlayColor: 'rgba(0, 0, 0, 0.5)',
-  primaryColor: '#f04',
-  spotlightShadow: '0 0 15px rgba(0, 0, 0, 0.5)',
-  textColor: '#333',
-  width: undefined,
-  zIndex: 100,
-};
-
   return (
     <header className="bg-primary-green">
       <Joyride callback={handleJoyrideCallback} steps={joyrideSteps} continuous={true} run={joyrideRun} showSkipButton={true} 
