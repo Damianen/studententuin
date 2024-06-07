e17a5d18b85faa4a2d5af9a179c8dc7a81a2c677
Coen <kruijf.coen@gmail.com>
2024-06-07 14:27:53 +0200

    add and fix upload and delete functionality watch out only for local use

--- src/portalComponents/Analytics.js
+++ src/portalComponents/Analytics.js
@@ -2,26 +2,47 @@ import React, { useState, useEffect } from "react";
 
 const Analytics = () => {
   const [logs, setLogs] = useState([]);
-  const usedStorage = 150;
-  const totalStorage = 300;
-  const storagePercentage = (usedStorage / totalStorage) * 100 
-  const storagePercentageString = Math.round(storagePercentage) + "%"    
-  useEffect(() => {}, []);
+  const totalStorage = 10000; // get this from database
 
+  const [usedStorage, setUsedStorage] = useState(0);
+  const [storagePercentage, setStoragePercentage] = useState(0);
+  const [storagePercentageString, setStoragePercentageString] = useState("0%");
+
+  useEffect(() => {
+    const fetchUsedStorage = async () => {
+      const response = await fetch("/dir-info", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log("dir size:" + data.size);
+        console.log("storage percentage:" + data.storagePercentage);
+        setUsedStorage(data.size); // Update usedStorage with the fetched size
+        setStoragePercentage(data.storagePercentage);
+        setStoragePercentageString(Math.round(data.storagePercentage) + "%");
+      }
+    };
+
+    fetchUsedStorage(); // Call the async function
+  }, []);
   return (
     <div>
-<hr className="h-px my-5 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+      <hr className="h-px my-5 bg-gray-200 border-0 dark:bg-gray-700"></hr>
       <h2>Storage usage</h2>
-      
+
       <div className="w-1/2 bg-gray-200 rounded-full h-8 mb-4 dark:bg-gray-700 flex-1">
         <div
           className="bg-gray-600 h-8 rounded-full dark:bg-gray-300 text-center text-black font-bold py-1"
-          style={{ width: `${storagePercentage}%`}}
-        ><div>
-        {storagePercentageString}
-      </div></div>
+          style={{ width: `${storagePercentage}%` }}
+        >
+          <div>{storagePercentageString}</div>
+        </div>
       </div>
-    <hr className="h-px my-5 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+      <hr className="h-px my-5 bg-gray-200 border-0 dark:bg-gray-700"></hr>
     </div>
   );
 };
