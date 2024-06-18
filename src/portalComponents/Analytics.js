cff475f066aeffc608884b21b3c9adcc10e4f35d
Coen <kruijf.coen@gmail.com>
2024-06-18 12:45:41 +0200

    added loading.. to stats

--- src/portalComponents/Analytics.js
+++ src/portalComponents/Analytics.js
@@ -8,6 +8,7 @@ const Analytics = () => {
     const [storagePercentage, setStoragePercentage] = useState(0);
     const [storagePercentageString, setStoragePercentageString] =
         useState("0%");
+    const [isLoaded, setIsLoaded] = useState(false);
 
     useEffect(() => {
         const fetchUsedStorage = async () => {
@@ -26,6 +27,7 @@ const Analytics = () => {
                 setStoragePercentageString(
                     Math.round(data.storagePercentage) + "%"
                 );
+                setIsLoaded(true);
             }
         };
 
@@ -35,16 +37,21 @@ const Analytics = () => {
         <div>
             <hr className="h-px my-5 bg-gray-200 border-0 dark:bg-gray-700"></hr>
             <h2>Opslag in gebruik</h2>
-
-            <div className="w-1/2 bg-gray-200 rounded-full h-8 mb-4 dark:bg-gray-700 flex-1">
-                <div
-                    className="bg-gray-600 h-8 rounded-full dark:bg-gray-300 text-center text-black font-bold py-1"
-                    style={{ width: `${storagePercentage}%` }}
-                >
-                    <div>{storagePercentageString}</div>
+            {isLoaded ? (
+                <p>Laden...</p>
+            ) : (
+                <div>
+                    <div className="w-1/2 bg-gray-200 rounded-full h-8 mb-4 dark:bg-gray-700 flex-1">
+                        <div
+                            className="bg-gray-600 h-8 rounded-full dark:bg-gray-300 text-center text-black font-bold py-1"
+                            style={{ width: `${storagePercentage}%` }}
+                        >
+                            <div>{storagePercentageString}</div>
+                        </div>
+                    </div>
+                    <hr className="h-px my-5 bg-gray-200 border-0 dark:bg-gray-700"></hr>
                 </div>
-            </div>
-            <hr className="h-px my-5 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+            )}
         </div>
     );
 };
